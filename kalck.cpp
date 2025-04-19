e2234d6cc262bc47aac54a2b917dfd6e27a8f36e
Flowizzz <nikonorovmakar29@gmail.com>
2025-04-19 10:22:29 +0300

    .

--- kalck.cpp
+++ kalck.cpp
@@ -1,1 +1,20 @@
+#include <iostream>
+using namespace std;
 
+int main() {
+    int spaceCount = 0;
+    char findCh = ' ';
+    string str;
+    
+    cout << "Введите строку: ";
+    getline(cin,str);
+    
+    for (int i = 0; i < str.length(); i++) {
+        if (str[i] == findCh) {
+            spaceCount++;
+        }
+    }
+    cout << endl;
+    cout << "Количество пробелов в строке:" << spaceCount;
+    return 0;
+}
