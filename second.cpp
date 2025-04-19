366039d51922bf012a0afd5169b01aaa780b6168
Flowizzz <nikonorovmakar29@gmail.com>
2025-04-19 10:54:55 +0300

    .

--- second.cpp
+++ second.cpp
@@ -1,1 +1,40 @@
+#include<iostream>
 
+using namespace std;
+
+int main()
+{
+    char contour, fill;
+    int width, height;
+    cout << "Введите символ для контура:\n";
+    cin >> contour;
+    cout << "Введите символ для заливки:\n";
+    cin >> fill;
+    cout << "Введите ширину:\n";
+    cin >> width;
+    cout << "Введите высоту:\n";
+    cin >> height;
+    if (height && width <= 0) {
+        cout << "Пожалуйста, введите положительные числа\n";
+        return 1;
+    }
+    for (int i = 0; i < width; i++) {
+        cout << contour; 
+    }
+      cout << endl;
+    for (int i = 1; i < height - 1; i++) {
+        cout << contour;
+        for (int j = 1; j < width - 1; j++) {
+        cout << fill;
+        }
+        cout << contour;
+        cout << endl;
+    }
+    if (height > 1){
+        for (int i = 0; i < width; i++){
+            cout << contour;
+        }
+        cout << endl;
+    }
+    return 0;
+}
